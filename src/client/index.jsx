1e984f28c8fcf07e407fd1e83053e5d81ce82139
Enrico Graziani <mrenrich84@gmail.com>
2017-03-28 22:08:10 +0100

    Bootstrap

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -20,6 +20,13 @@ import { isProd } from '../shared/util'
 
 import setUpSocket from './socket'
 
+import $ from 'jquery'
+import Tether from 'tether'
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
+
 // eslint-disable-next-line no-underscore-dangle
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
